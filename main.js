a6d4ab6787e77a27c078d27320e16203c76184c8
AhmedNov <ahmednecefov13@gmail.com>
2022-11-17 23:49:55 +0400

    second commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let colcBtn= document.getElementById('calculate')
 let res = document.getElementById('result')
 
 colcBtn.onclick = function() {
-    if (Number(num2.value)%2==0 & Number(num1.value)%2==0) {
+    if (Number(num1.value)%2==0 & Number(num2.value)%2==0) {
         let total =Math.pow( Number(num1.value) ,2) +  Math.pow(Number(num2.value) ,2)
     res.innerHTML= total 
     }
