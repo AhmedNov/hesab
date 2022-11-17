0ab1b2322c05848cb3f68a94a823d5ef65988203
AhmedNov <ahmednecefov13@gmail.com>
2022-11-17 23:26:39 +0400

    cut reqemler

--- main.js
+++ main.js
@@ -4,11 +4,22 @@ let colcBtn= document.getElementById('calculate')
 let res = document.getElementById('result')
 
 colcBtn.onclick = function() {
+    if (Number(num1.value)%2==0) {
+        let total =Math.pow( Number(num1.value) ,2) +  Math.pow(Number(num2.value) ,2)
+    res.innerHTML= total 
+    }
+    else if(Number(num2.value)%2==0) {
+        let total =Math.pow( Number(num1.value) ,2) +  Math.pow(Number(num2.value) ,2)
+    res.innerHTML= total 
+    }
+
+    else{
+        alert('sehv')
+    }
     
     // let total = (num1.value*num1.value) + (num2.value*num2.value)
     
-    let total = Math.pow( Number(num1.value) ,2) +  Math.pow(Number(num2.value) ,2)
-    res.innerHTML= total
+   
 }
 
 
