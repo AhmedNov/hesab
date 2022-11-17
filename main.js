5b330c354e972865aabec3719824ecc342509c95
AhmedNov <ahmednecefov13@gmail.com>
2022-11-17 23:48:18 +0400

    second commit

--- main.js
+++ main.js
@@ -4,22 +4,14 @@ let colcBtn= document.getElementById('calculate')
 let res = document.getElementById('result')
 
 colcBtn.onclick = function() {
-    if (Number(num1.value)%2==0) {
-        let total =Math.pow( Number(num1.value) ,2) +  Math.pow(Number(num2.value) ,2)
-    res.innerHTML= total 
-    }
-    else if(Number(num2.value)%2==0) {
+    if (Number(num2.value)%2==0 & Number(num1.value)%2==0) {
         let total =Math.pow( Number(num1.value) ,2) +  Math.pow(Number(num2.value) ,2)
     res.innerHTML= total 
     }
 
     else{
         alert('sehv')
+        
     }
-    
-    // let total = (num1.value*num1.value) + (num2.value*num2.value)
-    
-   
 }
-
 
