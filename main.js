d0c53c623288ea03021128d323695d837c57e918
AhmedNov <ahmednecefov13@gmail.com>
2022-11-18 19:00:32 +0400

    first commit

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ let num2 = document.getElementById('n2')
 let calcBtn= document.getElementById('calculate')
 let res = document.getElementById('result')
 
+
 calcBtn.onclick = function() {
+
+
     if (Number(num1.value)%2==0 & Number(num2.value)%2==0) {
         let total =Math.pow( Number(num1.value) ,2) +  Math.pow(Number(num2.value) ,2)
     res.innerHTML= total 
@@ -15,3 +18,16 @@ calcBtn.onclick = function() {
     }
 }
 
+    var inputs = document.querySelectorAll("input,select");
+for (var i = 0 ; i < inputs.length; i++) {
+   inputs[i].addEventListener("keypress", function(e){
+      if (e.which == 13) {
+         e.preventDefault();
+         var nextInput = document.querySelectorAll('[tabIndex="' + (this.tabIndex + 1) + '"]');
+         if (nextInput.length === 0) {
+            calcBtn.onclick ();
+         }
+         nextInput[0].focus();
+      }
+   })
+}
